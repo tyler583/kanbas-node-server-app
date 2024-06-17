094e37f4ab503f13add87c00e25865d03674b47e
Tyler Lam <tylerlam@Tylers-MacBook-Pro.local>
2024-06-17 18:39:01 -0400

    fixed a6

--- App.js
+++ App.js
@@ -5,6 +5,12 @@ import cors from "cors";
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import mongoose from "mongoose";
+import "dotenv/config";
+import UserRoutes from './Users/routes.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors()); 
@@ -15,5 +21,6 @@ CourseRoutes(app);
 ModuleRoutes(app);
 Hello(app);
 Lab5(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
