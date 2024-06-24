4883e7c533cb2a15b49e428521aeda0aa9ada366
Tyler Lam <tylerlam@Tylers-MacBook-Pro.local>
2024-06-24 19:24:24 -0400

    a6 mongo fix1

--- Users/routes.js
+++ Users/routes.js
@@ -1,5 +1,5 @@
 import * as dao from "./dao.js";
-let currentUser = null;
+//let currentUser = null;
 export default function UserRoutes(app) {
   const createUser = async (req, res) => {
     const user = await dao.createUser(req.body);
